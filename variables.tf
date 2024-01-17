0f5e15a411b3b68a25a4b2cd1f3e504809536008
SANKETC1999 <80206579+SANKETC1999@users.noreply.github.com>
2024-01-17 23:33:50 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "batch21" 
+    default     =  "Sanket-keypair" 
 }
 
 variable "instance_type" { 
